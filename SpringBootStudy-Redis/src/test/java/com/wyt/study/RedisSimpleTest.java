8c90d32e41fd7bc9cd122c2f6e69ee1bfaaf496c
wyt10242 <wyt10242>
2020-06-01 15:49:36 +0800

    初始化

---
+++ SpringBootStudy-Redis/src/test/java/com/wyt/study/RedisSimpleTest.java
@@ -0,0 +1,21 @@
+package com.wyt.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisSimpleTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test(){
+        redisTemplate.opsForValue().get("test");
+    }
+}
